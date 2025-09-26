b048a59f3c1c4549a51af981b3e3db48e53c97a3
junior-boop <juniorseppo3@gmail.com>
2025-09-26 01:08:23 +0100

    Add database model definitions for users, notes, articles, comments, groups, appreciations, devices, and sync events

---
+++ utils/function.ts
@@ -0,0 +1,12 @@
+import { SimpleORM, ModelFactory } from "./simpleorm";
+
+type Env = Partial<CloudflareBindings>;
+
+const db = (env: Env) => {
+  const orm = new SimpleORM(env.DB as D1Database);
+  const database = new ModelFactory(orm);
+
+  return database;
+};
+
+export default db;
